b7cde2b54b937e7d24629fee38964a9f991ed0f2
Your Name <you@example.com>
2024-09-19 23:44:55 -0700

    adding admin

--- gol/envahed/admin.py
+++ gol/envahed/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
+from django.contrib import admin 
+from .models import  *
+# Register your models here.
+@admin.register (Ostad)
+class OstadAdmin(admin.ModelAdmin): ...
+
+@admin.register (Student)
+class StudentAdmin(admin.ModelAdmin): ...
+
+@admin.register (Dars)
+class DarsAdmin(admin.ModelAdmin): ...
+
 
 # Register your models here.
