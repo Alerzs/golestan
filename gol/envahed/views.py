074bb70adbc3bc9d017045d82644401de88f7f95
Alireza Shams <alerzs1023@gmail.com>
2024-09-20 10:21:38 +0330

    edwf

--- gol/envahed/views.py
+++ gol/envahed/views.py
@@ -1,2 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 
