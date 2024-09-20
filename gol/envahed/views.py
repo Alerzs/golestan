420704ec43dbcd303b5865c7667cb5193bbbe7d2
rozhrafiee <rozhinarafiee014@gmail.com>
2024-09-20 11:36:24 +0430

    show_dars add_dars

--- gol/envahed/views.py
+++ gol/envahed/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Ostad, Dars, Student
+from .forms import OstadForm, DarsForm, StudentForm
+import requests
 
-# Create your views here.
+
+def add_dars(request):
+    form = DarsForm()
+    if request.method == "POST":
+        form = DarsForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return render(request,"envahed/add_dars.html", {"form": form})  
+      
+def show_dars(request):
+    form = DarsForm()
+    my_student = Student.objects.get(user = request.user)
+    dars = my_student.dars.all()
+    return render(request,"envahed/show_dars.html", {"dars": dars})
+
