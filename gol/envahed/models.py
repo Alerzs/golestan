c0f81e49804d7a01931c1c31ba93ca0ae501e2d7
Alireza Shams <alerzs1023@gmail.com>
2024-09-20 10:11:55 +0330

    dfghjkl;

--- gol/envahed/models.py
+++ gol/envahed/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+
+class Ostad(models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE)
+
+class Dars(models.Model):
+    vahed = models.IntegerField()
+    ostad = models.ForeignKey(Ostad ,on_delete=models.CASCADE)
+    rooz = models.DateTimeField()
+    cap = models.PositiveIntegerField()
+    reshte = models.CharField(max_length=25)
+    
+
+class Student(models.Model):
+    user = models.OneToOneField(User ,on_delete=models.CASCADE)
+    reshte = models.CharField(max_length=20)
+    dars = models.ManyToManyField(Dars)
