da6d351f50fa854064b0864124747d4925bdcf34
Alireza Shams <alerzs1023@gmail.com>
2024-09-20 10:21:38 +0330

    edit dars

--- gol/envahed/views.py
+++ gol/envahed/views.py
@@ -1,2 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+
+from django.shortcuts import render
+from .models import *
+from .forms import *
+def edit_dars(request , dars_id):
+     dars = Dars.objects.get(id=dars_id)
+     if request.method == 'POST':
+         form = DarsForm(request.POST , instance= dars)
+         if form.is_valid():
+             form.save()
+             return 
+
 
